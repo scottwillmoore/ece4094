6d8ee3c35f41003d46ffb3eb69de9c8a36ad3f32
Scott Moore <scott.will.moore@gmail.com>
2021-08-03 13:30:03 +1000

    Install React and setup the stylelint-order plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
-import "./reset.css";
 import "./index.css";
 
-console.log("Hello, world!");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<p>Hello, world!</p>, root);
---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+defineConfig({
+	plugins: [reactRefresh()],
+});
